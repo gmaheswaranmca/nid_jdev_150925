d107dd59a92cd87965c222483b11bf92098750b0
gmaheswaranmca <gmaheswaranmca@gmail.com>
2025-09-19 14:36:02 +0530

    nie jdev

--- ecom/client/src/admin/product/ProductCreate.jsx
+++ ecom/client/src/admin/product/ProductCreate.jsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
 export default function ProductCreate() {
+    const [product, setProduct] = useState({id:'', name: '', description:"",
+         category:"", 
+         tags:"",
+         stock:1, price:0});
+    const navigate = useNavigate();
+    const handleCreateProduct = async () => {
+        const baseUrl = 'http://localhost:8081';
+        const response = await axios.post(`${baseUrl}/products/save`,{...product});
+        alert('Product Created Successfully.');
+        navigate('/admin');
+    };
     return (
         <>
             <h3>Create Product</h3>
@@ -6,30 +21,43 @@ export default function ProductCreate() {
                 <form>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" />
+                        <input type="text" className="form-control" id="name" 
+                         value={product.name} onChange={(event) => { setProduct({...product, name:event.target.value}); } } 
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description" className="form-label">Description</label>
-                        <input type="text" className="form-control" id="description" />
+                        <input type="text" className="form-control" id="description" 
+                        value={product.description} onChange={(event) => { setProduct({...product, description:event.target.value}); } }
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="category" className="form-label">Cateogory</label>
-                        <input type="text" className="form-control" id="category" />
+                        <input type="text" className="form-control" id="category" 
+                        value={product.category} onChange={(event) => { setProduct({...product, category:event.target.value}); } }
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tags" className="form-label">Tags</label>
-                        <input type="text" className="form-control" id="tags" />
+                        <input type="text" className="form-control" id="tags" 
+                        value={product.tags} onChange={(event) => { setProduct({...product, tags:event.target.value}); } }
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="stock" className="form-label">Stock</label>
-                        <input type="text" className="form-control" id="stock" />
+                        <input type="text" className="form-control" id="stock" 
+                        value={product.stock} onChange={(event) => { setProduct({...product, stock:event.target.value}); } }
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="price" className="form-label">Price</label>
-                        <input type="text" className="form-control" id="price" />
+                        <input type="text" className="form-control" id="price" 
+                        value={product.price} onChange={(event) => { setProduct({...product, price:event.target.value}); } }
+                        />
                     </div>
 
-                    <button type="submit" className="btn btn-primary">Create Product</button>
+                    <button type="button" className="btn btn-primary"
+                        onClick={handleCreateProduct}>Create Product</button>
                 </form>
             </div>
         </>
